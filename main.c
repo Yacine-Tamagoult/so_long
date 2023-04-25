4f602bc3a59da9f659ae82230428be5cdbe935d9
Ilona SELBONNE <ilselbon@e2r13p4.clusters.42paris.fr>
2023-04-25 19:50:54 +0200

    ilo

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: soleil <soleil@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ilselbon <ilselbon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 21:56:35 by soleil            #+#    #+#             */
-/*   Updated: 2023/04/25 17:02:06 by soleil           ###   ########.fr       */
+/*   Updated: 2023/04/25 19:47:17 by ilselbon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	**ft_copy_map(char **tab)
 		return (NULL);
 	while (tab[i])
 		i++;
-	copy = (char **)malloc((i + 1) * sizeof(char *));
+`
 	if (!copy)
 		return (NULL);
 	copy[i] = NULL;
@@ -39,6 +39,13 @@ char	**ft_copy_map(char **tab)
 		}
 		j++;
 	}
+	int o = 0;
+	while(tab[o])
+	{
+		free(tab[o]);
+		o++;
+	}
+	free(tab);
 	return (copy);
 }
 
@@ -56,10 +63,7 @@ char	**ft_maps(char *str, char **tab)
 	while (j >= 0)
 	{
 		k = 0;
-		map[j] = malloc(sizeof(char) * (ftstrlen(tab[j]) + 1));
-		while (tab[j][k])
-			map[j][k++] = tab[j][k];
-		map[j][k] = 0;
+		map[j] = strdup(tab[j]);
 		j--;
 	}
 	k = 0;
@@ -98,6 +102,7 @@ char	**ft_fd_init(char *file)
 	int		i;
 	char	*str;
 	char	**tab;
+	char	**tmp;
 	int		fd;
 
 	i = 0;
@@ -118,6 +123,7 @@ char	**ft_fd_init(char *file)
 int main(int ac, char **av)
 {
     t_vars vars;
+	char **tab;
     int j;
     int k;
 
@@ -128,12 +134,13 @@ int main(int ac, char **av)
     if (ft_check_ber(av[1]))
         return (printf("Error\n"), 1);
     memset(&vars, 0, sizeof(vars));
-    vars.map = ft_copy_map(ft_fd_init(av[1]));
+	tab = ft_fd_init(av[1]);
+    vars.map = ft_copy_map(tab);
     if (!vars.map)
         return (printf("error"), 1);
     vars.check_map = ft_copy_map(vars.map);
     if (ft_check_master(vars.check_map, &vars))
-        return (free_map(vars.map), free_map(vars.check_map), 1);
+		return ( free_map(vars.map), free_map(vars.check_map), 1);
     ft_count(vars.check_map, &vars);
     vars.mlx = mlx_init();
     vars.win = mlx_new_window(vars.mlx, vars.col * 50,
@@ -148,4 +155,3 @@ int main(int ac, char **av)
     vars.check_map = NULL;
     return (0);
 }
-
